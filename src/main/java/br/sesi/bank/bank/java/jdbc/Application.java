6224a33c28374131236abe386a69c947556aef78
thalleslopes12 <thallesmerencio12@gmail.com>
2024-10-11 09:58:01 -0300

    Subindo projeto padrão

---
+++ src/main/java/br/sesi/bank/bank/java/jdbc/Application.java
@@ -0,0 +1,13 @@
+package br.sesi.bank.bank.java.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
